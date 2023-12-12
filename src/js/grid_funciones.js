9ae766e7a4eb1c262e9d53bc5d9b375514a34997
IvanBadia2003 <ibadiamiguel@gmail.com>
2023-12-12 01:42:35 +0100

    funciona el filtro

--- src/js/grid_funciones.js
+++ src/js/grid_funciones.js
@@ -2,6 +2,7 @@
 const iconFilter = document.getElementById('iconFilter');
 const close = document.getElementById('close');
 const filtros = document.getElementById('sidebar');
+const checkboxes = document.querySelectorAll('.genre'); // Obtener los checkboxes de género
 
 // Función para mostrar el menú correspondiente a la opción seleccionada
 function mostrarFiltros() {
@@ -11,6 +12,7 @@ function mostrarFiltros() {
     filtros.style.display = 'block';
   }
 }
+
 // Función para cerrar el menú correspondiente a la opción seleccionada
 function cerrarFiltros() {
   if (filtros.style.display === 'none') {
@@ -20,9 +22,12 @@ function cerrarFiltros() {
   }
 }
 
+// Función para filtrar funciones por género
+function filtrarPorGenero() {
+  const selectedGenres = Array.from(checkboxes)
+    .filter(checkbox => checkbox.checked)
+    .map(checkbox => checkbox.value);
 
-
-document.addEventListener('DOMContentLoaded', () => {
   const url = 'http://localhost:3000/obras'; // URL de la API de las funciones
 
   fetch(url)
@@ -33,49 +38,93 @@ document.addEventListener('DOMContentLoaded', () => {
       return response.json();
     })
     .then(data => {
-      // Obtener el contenedor de funciones
-      const container = document.querySelector('.content .functions'); // Cambiado el selector
+      const container = document.querySelector('.functions'); // Obtener el contenedor de funciones
 
-      // Recorrer todas las funciones y crear elementos HTML para cada una
-      data.forEach(funcion => {
-        const article = document.createElement('article');
-        article.classList.add('card');
-      
-        const anchor = document.createElement('a');
-        anchor.href = `descripcion_funcion.html?id=${funcion.id}`; // Incluyendo el ID en la URL
-      
-        const imgContainer = document.createElement('div');
-        imgContainer.classList.add('imgContainer');
-      
-        const img = document.createElement('img');
-        img.src = funcion.imagenObra;
-        img.alt = funcion.titulo;
-      
-        imgContainer.appendChild(img);
-        anchor.appendChild(imgContainer);
-      
-        const textContainer = document.createElement('div');
-        textContainer.classList.add('card__description');
-      
-        const h2 = document.createElement('h2');
-        h2.textContent = funcion.titulo;
-      
-        const h3 = document.createElement('h3');
-        h3.textContent = funcion.diaObra;
-      
-        textContainer.appendChild(h2);
-        textContainer.appendChild(h3);
-      
-        anchor.appendChild(textContainer);
-        article.appendChild(anchor);
-      
-        container.appendChild(article);
-      });
-      
+      // Verificar si no se ha seleccionado ningún género
+      if (selectedGenres.length === 0) {
+        // Mostrar todas las funciones si no hay géneros seleccionados
+        data.forEach(funcion => {
+          const article = document.createElement('article');
+          article.classList.add('card');
+        
+          const anchor = document.createElement('a');
+          anchor.href = `descripcion_funcion.html?id=${funcion.id}`; // Incluyendo el ID en la URL
+        
+          const imgContainer = document.createElement('div');
+          imgContainer.classList.add('imgContainer');
+        
+          const img = document.createElement('img');
+          img.src = funcion.imagenObra;
+          img.alt = funcion.titulo;
+        
+          imgContainer.appendChild(img);
+          anchor.appendChild(imgContainer);
+        
+          const textContainer = document.createElement('div');
+          textContainer.classList.add('card__description');
+        
+          const h2 = document.createElement('h2');
+          h2.textContent = funcion.titulo;
+        
+          const h3 = document.createElement('h3');
+          h3.textContent = funcion.diaObra;
+        
+          textContainer.appendChild(h2);
+          textContainer.appendChild(h3);
+        
+          anchor.appendChild(textContainer);
+          article.appendChild(anchor);
+        
+          container.appendChild(article);
+        });
+      } else {
+        // Filtrar las funciones por género seleccionado
+        const filteredFunctions = data.filter(funcion => selectedGenres.includes(funcion.genero));
+
+        // Recorrer funciones filtradas y crear elementos HTML para cada una
+        filteredFunctions.forEach(funcion => {
+          const article = document.createElement('article');
+          article.classList.add('card');
+        
+          const anchor = document.createElement('a');
+          anchor.href = `descripcion_funcion.html?id=${funcion.id}`; // Incluyendo el ID en la URL
+        
+          const imgContainer = document.createElement('div');
+          imgContainer.classList.add('imgContainer');
+        
+          const img = document.createElement('img');
+          img.src = funcion.imagenObra;
+          img.alt = funcion.titulo;
+        
+          imgContainer.appendChild(img);
+          anchor.appendChild(imgContainer);
+        
+          const textContainer = document.createElement('div');
+          textContainer.classList.add('card__description');
+        
+          const h2 = document.createElement('h2');
+          h2.textContent = funcion.titulo;
+        
+          const h3 = document.createElement('h3');
+          h3.textContent = funcion.diaObra;
+        
+          textContainer.appendChild(h2);
+          textContainer.appendChild(h3);
+        
+          anchor.appendChild(textContainer);
+          article.appendChild(anchor);
+        
+          container.appendChild(article);
+        });
+      }
     })
     .catch(error => console.error('There has been a problem with your fetch operation:', error));
+}
+
+// Agregar event listener a cada checkbox para el filtro por género
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', filtrarPorGenero);
 });
 
-
-
-
+// Llamar a la función para mostrar todas las funciones al cargar la página
+document.addEventListener('DOMContentLoaded', filtrarPorGenero);
